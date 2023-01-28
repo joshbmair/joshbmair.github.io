5c2d006eeb8d4ea7bbf6ce65b93bb629b0b37e5b
Josh Birlingmair <jcbmair@gmail.com>
2023-01-28 13:21:25 -0600

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Josh Birlingmair`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
