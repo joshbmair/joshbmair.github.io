b78b990258fa80071a9bf213fdc19f55ef2d105a
Josh Birlingmair <jcbmair@gmail.com>
2023-01-30 22:21:10 -0600

    Add more to welcome component

--- src/components/sections/Welcome.js
+++ src/components/sections/Welcome.js
@@ -4,7 +4,7 @@ export default function Welcome() {
   return (
     <>
       <h1>Hi, I'm Josh Birlingmair</h1>
-      <p>Welcome to my personal site!</p>
+      <p>Welcome to my personal site. Scroll down to learn more about me and browse my work experience.</p>
     </>
   );
 }
