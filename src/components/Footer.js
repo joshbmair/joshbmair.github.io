a926016a4cc1747250d0382ec9b5ba4049b65965
Josh Birlingmair <jcbmair@gmail.com>
2023-02-01 13:21:29 -0600

    Reduce duplicated code in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,25 @@
 import * as React from "react";
 import { BsInstagram, BsFacebook, BsLinkedin, BsGithub } from "react-icons/bs";
 
+const socialProfiles = [
+  {
+    href: "https://github.com/joshbmair",
+    icon: <BsGithub />,
+  },
+  {
+    href: "https://www.linkedin.com/in/josh-birlingmair-46b85317b",
+    icon: <BsLinkedin />,
+  },
+  {
+    href: "https://www.instagram.com/josh.bmr",
+    icon: <BsInstagram />,
+  },
+  {
+    href: "https://www.facebook.com/josh.birlingmair",
+    icon: <BsFacebook />,
+  },
+];
+
 export default function Footer() {
   return (
     <footer>
@@ -8,22 +27,14 @@ export default function Footer() {
         <small>This site was built by Josh Birlingmair using Gatsby.js</small>
       </p>
       <p>
-        <LinkedIcon href="https://github.com/joshbmair" icon={<BsGithub />} />
-        &nbsp;
-        <LinkedIcon
-          href="https://www.linkedin.com/in/josh-birlingmair-46b85317b"
-          icon={<BsLinkedin />}
-        />
-        &nbsp;
-        <LinkedIcon
-          href="https://www.instagram.com/josh.bmr"
-          icon={<BsInstagram />}
-        />
-        &nbsp;
-        <LinkedIcon
-          href="https://www.facebook.com/josh.birlingmair"
-          icon={<BsFacebook />}
-        />
+        {socialProfiles.map((profile) => {
+          return (
+            <LinkedIcon
+              href={profile.href}
+              icon={profile.icon}
+            />
+          );
+        })}
       </p>
     </footer>
   );
@@ -31,7 +42,7 @@ export default function Footer() {
 
 function LinkedIcon({ icon, href }) {
   return (
-    <a href={href} target="_blank">
+    <a className="linked-icon" href={href} target="_blank">
       {icon}
     </a>
   );
