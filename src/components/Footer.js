3f6dac9e5db23d81d5d7d880f38a587a307da8fd
Josh Birlingmair <jcbmair@gmail.com>
2023-02-11 22:13:45 -0600

    Use key in list of social profiles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,18 +5,22 @@ import "../styles/footer.css";
 
 const socialProfiles = [
   {
+    key: 1,
     href: "https://github.com/joshbmair",
     icon: <BsGithub />,
   },
   {
+    key: 2,
     href: "https://www.linkedin.com/in/josh-birlingmair-46b85317b",
     icon: <BsLinkedin />,
   },
   {
+    key: 3,
     href: "https://www.instagram.com/josh.bmr",
     icon: <BsInstagram />,
   },
   {
+    key: 4,
     href: "https://www.facebook.com/josh.birlingmair",
     icon: <BsFacebook />,
   },
@@ -28,18 +32,18 @@ export default function Footer() {
       <p>
         <small>This site was built by Josh Birlingmair using Gatsby.js</small>
       </p>
-      <p>
+      <p >
         {socialProfiles.map((profile) => {
-          return <LinkedIcon href={profile.href} icon={profile.icon} />;
+          return <LinkedIcon href={profile.href} icon={profile.icon} key={profile.key} />;
         })}
       </p>
     </footer>
   );
 }
 
-function LinkedIcon({ icon, href }) {
+function LinkedIcon({ href, icon }) {
   return (
-    <a className="linked-icon" href={href} target="_blank" rel="noreferrer" >
+    <a className="linked-icon" href={href} rel="noreferrer" target="_blank" >
       {icon}
     </a>
   );
