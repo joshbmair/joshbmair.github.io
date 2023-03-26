5a483c5b2f7852006bc79fcf5015f927bb637b94
Josh Birlingmair <jcbmair@gmail.com>
2023-03-26 13:04:14 -0500

    Update the About section

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -10,20 +10,17 @@ export default function About() {
       </h1>
       <div className="section-summary">
         <h2>
-          I just received a Bachelor of Science degree in Computer Science from
-          Colorado State University
+          I received a Bachelor of Science degree in Computer Science from
+          Colorado State University in 2022
         </h2>
         <p>
           Colorado State University is located in Fort Collins, Colorado. That
           happens to also be my hometown!
         </p>
-        <h2>
-          Now, I'm about to start my job as a software developer for Epic
-          Systems.
-        </h2>
+        <h2>Now, I'm working as a software developer for Epic.</h2>
         <p>
-          Epic is located in Verona, Wisconsin and develops healthcare software.
-          I'm looking forward to the experience of a new town and a new job!
+          Epic is located in Verona, Wisconsin and develops software for healthcare organizations.
+          So far, I'm learning lots of new skills and I'm excited to get more experience!
         </p>
         <h2>
           Before I got my job at Epic, I was an IT specialist/GIS analyst intern
